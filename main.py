5726c457ce129370a4ea600f859ea4fb92ec11b9
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2022-02-07 11:20:29 -0500

    add

---
+++ main.py
@@ -0,0 +1,14 @@
+#author: LM (AMDG) 2/7/22
+# Tic Tac Toe project
+# We need to make a board to play on
+# Need to make X's and O's as markers
+# Figure out hpw to place them
+    #If somebody got three in a row
+
+
+#design of board 
+# use list to design
+board = ['-', '-', '-',
+         '-', '-', '-',
+         '-', '-', '-']
+# use function to display board, add 'l' as border
