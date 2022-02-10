31fea139a3d12e54cadd25090e0b133659448dc7
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2022-02-10 11:14:16 -0500

    add file

--- main.py
+++ main.py
@@ -5,24 +5,39 @@
 # Figure out hpw to place them
     #If somebody got three in a row
 
-while True:
-    try:
-
-        yesorno = input("Do you wish to play? If yes enter YES. If no enter NO: ").capitalize
-        if yesorno == 'YES':
-            continue
-        elif yesorno == 'NO':
-            break
+    #design of board 
+# use list to design board
+from turtle import pos
 
 
-#design of board 
-# use list to design
 board = ['-', '-', '-',
          '-', '-', '-',
          '-', '-', '-']
 # use function to display board, add 'l' as border
 def board_game():
-    print(board[0] + 'l' + board[1] + 'l' + board[2])
-    print(board[3] + 'l' + board[4] + 'l' + board[5])
-    print(board[6] + 'l' + board[7] + 'l' + board[8])
+    print(board[0] + ' l ' + board[1] + ' l ' + board[2])
+    print(board[3] + ' l ' + board[4] + ' l ' + board[5])
+    print(board[6] + ' l ' + board[7] + ' l ' + board[8])
+def playing():
+    yesorno = str(input("Do you wish to play? Enter YES if yes or NO if no: ")).upper()
+    if yesorno == 'YES':
+       board_game()
+       def player_turn():
+            player_one_position = int(input("Enter a number from one to nine: "))
+            player_one_position = player_one_position - 1
+            board[player_one_position] = 'X'
+            board_game()
+            player_turn()
+    elif yesorno == 'NO':
+        print("Have a nice day!")
+
+
+
+
+
+
+    
+
+playing()
+
 
