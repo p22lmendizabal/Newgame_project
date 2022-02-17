5781955391c74d04d118aa8c96d72e7e822218bb
p22lmendizabal <p22lmendizabal@fairfieldprep.org>
2022-02-17 10:16:35 -0500

    add file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-#author: LM (AMDG) 2/7/22
-# Tic Tac Toe project
+#author: LM (AMDG) 2/17/22
+#Tic tac toe project 
 # We need to make a board to play on
 # Need to make X's and O's as markers
 # Figure out hpw to place them
@@ -11,36 +11,38 @@
 board = ['-', '-', '-',
          '-', '-', '-',
          '-', '-', '-']
+#there is no winner but it will be updated later
+winner = None
+currentplayer = 'O'
+gameisgoingon = True
 # use function to display board, add 'l' as border
 def board_game():
     print(board[0] + ' l ' + board[1] + ' l ' + board[2])
     print(board[3] + ' l ' + board[4] + ' l ' + board[5])
     print(board[6] + ' l ' + board[7] + ' l ' + board[8])
-
-#this is taking a turn from player    
-def player_turn():
-    while True:
-        try:
-            player_one_position = int(input("Player one, enter a number from one to nine: "))
-            player_one_position = player_one_position - 1
-            if board[player_one_position] == '-':
-                board[player_one_position] = 'O'
-            elif board[player_one_position] == 'X':
+#the main game
+def playgame():
+    while gameisgoingon: 
+        #this is taking a turn from player    
+        def player_turn():
+            currentplayer = int(input("Current player, enter a number from one to nine: "))
+            currentplayer = currentplayer - 1
+            if board[currentplayer] == '-':
+                board[currentplayer] = currentplayer
+                board_game()
+            elif board[currentplayer] == 'X':
                 print("That position was already taken")
-                player_one_position
-            board_game()
-            player_two_position = int(input("Player two, enter a number from one to nine: "))
-            player_two_position = player_two_position - 1
-            if board[player_two_position] == '-':
-                board[player_two_position] = 'X'
-            elif board[player_two_position] == 'O':
-            
-        except:
-            print("That spot is already taken")
+                board_game()  
+        player_turn()
 
-        board_game()
-player_turn()
-
+        def flipplayer():
+            if currentplayer == 'O':
+                currentplayer == 'X'
+            elif currentplayer == 'X':
+                currentplayer == 'O'
+                return
+        flipplayer()
+playgame()
 #checks if a player won or if the game is a tie
 
 
